4ef523b1e381c0eefb5242a03c21d14442a1f758
paulohenrique64 <paulohenriquelvs20@gmail.com>
2024-10-26 00:08:29 -0300

    Criadas as classes A e B utilizando a linguagem Java

---
+++ A.java
@@ -0,0 +1,28 @@
+public class A {
+    public int A1;
+    public float A2;
+
+    public int getA1() {
+        return A1;
+    }
+
+    public void setA1(int a1) {
+        A1 = a1;
+    }
+
+    public float getA2() {
+        return A2;
+    }
+
+    public void setA2(float a2) {
+        A2 = a2;
+    }
+
+    public void MA1() {
+        System.out.println("MA1");
+    } 
+    
+    public void MA2() {
+        System.out.println("MA2");
+    }
+}
