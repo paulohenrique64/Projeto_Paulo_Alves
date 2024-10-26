32ad5bdabd71242abe7be81276b09755fb7769e6
paulohenrique64 <paulohenriquelvs20@gmail.com>
2024-10-26 01:27:41 -0300

    Criada a nova classe D e implementados os metodos MD1 e MD2 na classe D

---
+++ D.java
@@ -0,0 +1,28 @@
+public class D {
+    private String D1;
+    private int D2;
+
+    public String getD1() {
+        return D1;
+    }
+
+    public void setD1(String d1) {
+        D1 = d1;
+    }
+
+    public int getD2() {
+        return D2;
+    }
+
+    public void setD2(int d2) {
+        D2 = d2;
+    }
+
+    public void MD1() {
+        System.out.println("hello world");
+    } 
+    
+    public int MD2() {
+        return D2 * D2;
+    }
+}
